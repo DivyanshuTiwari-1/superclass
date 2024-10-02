ce0d219358317f6a50dc28a0d9e0470b55cc4a02
DiyanshuTiwaari <sontitiwa569@gamil.com>
2024-10-02 17:47:50 +0530

    added mongoose

--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -2,20 +2,24 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function SignIn() {
-  const { data: session } = useSession();
+    const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        <p>Signed in as {session.user.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+    if (session) {
+        return ( <
+            >
+            <
+            p > Signed in as { session.user.email } < /p> <
+            button onClick = {
+                () => signOut() } > Sign out < /button> <
+            />
+        );
+    }
+    return ( <
+        >
+        <
+        p > Not signed in < /p> <
+        button onClick = {
+            () => signIn("google") } > Sign in with Google < /button> <
+        />
     );
-  }
-  return (
-    <>
-      <p>Not signed in</p>
-      <button onClick={() => signIn("google")}>Sign in with Google</button>
-    </>
-  );
 }
