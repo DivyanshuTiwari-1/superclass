92406b55b21fe951423cc4509417e6498173d400
Divyanshu Tiwari <128217067+DivyanshuTiwari-1@users.noreply.github.com>
2024-10-03 19:53:13 +0530

    Update page.jsx

--- src/app/components/Navbar/page.jsx
+++ src/app/components/Navbar/page.jsx
@@ -61,7 +61,7 @@ export default function Navbar(){
         <nav className="bg-white dark:bg-gray-900 fixed w-full  top-0 start-0 border-b border-gray-200 dark:border-gray-600">
            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-               <Image src="https://t3.ftcdn.net/jpg/04/34/37/76/360_F_434377688_1RQdwGB8IjGPyuNsQgEcp0yGVYDR8yPz.jpg" className="h-8" />
+               <Image src="https://t3.ftcdn.net/jpg/04/34/37/76/360_F_434377688_1RQdwGB8IjGPyuNsQgEcp0yGVYDR8yPz.jpg" className="h-8" alt="Flowbite Logo"/>
                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Superclass</span>
            </a>
            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
