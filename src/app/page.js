563660107f20fdbb5d034e83e308c5475ce03cbc
Divyanshu Tiwari <128217067+DivyanshuTiwari-1@users.noreply.github.com>
2024-10-09 11:26:36 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar/page";
 import Course from "./components/Course/page";
-import Footer from "./components/Footer/page";
+import Footer from "./components/footer/page";
 
 
 export default function Home() {
-    return ( <
-        >
-        <
-        Navbar > < /Navbar> <
-        div className = "min-h-screen bg-black flex items-center justify-center mt-20" >
-        <
-        Course / >
+    return ( <>
+        
+       <Navbar> </Navbar>
+        <div className = "min-h-screen bg-black flex items-center justify-center mt-20">
+        
+        <Course/>
 
-        <
-        /div> <
-        Footer / >
-        <
-        />
+        
+        </div> 
+        <Footer/>
+        
+        </>
     );
 }
