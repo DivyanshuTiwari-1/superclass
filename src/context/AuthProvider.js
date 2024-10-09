f611b221729d378f6375c9e0ea9123438901192f
DiyanshuTiwaari <sontitiwa569@gmail.com>
2024-10-09 09:38:23 +0530

    changes made

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,9 +1,10 @@
-"use client"; // Mark as a client component since SessionProvider relies on client-side session handling
+"use client";  // Mark as a client component since SessionProvider relies on client-side session handling
 import { SessionProvider } from "next-auth/react";
 
 export default function AuthProvider({ children }) {
-    return ( <
-        SessionProvider > { children } <
-        /SessionProvider>
-    );
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  );
 }
