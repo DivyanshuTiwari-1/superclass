2d6bc3c31b2e2842abd607936068bba2160d1645
DiyanshuTiwaari <sontitiwa569@gmail.com>
2024-10-11 17:33:08 +0530

    ddddd

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,13 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
 <<<<<<< HEAD
     return (
-        <html lang = "en" >
-        
-        <AuthProvider>
-        
-       <body className="bg-black"> { children } 
-        </body> 
-       </AuthProvider>
+        <html lang="en" className={myFont.variable}>
+            <AuthProvider>
+                <body className="bg-black">
+                    {children}
+                </body>
+            </AuthProvider>
 =======
     return ( 
 <html lang = "en">
