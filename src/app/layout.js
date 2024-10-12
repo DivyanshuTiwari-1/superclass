f0ef4b02e75329bda6146862d9c40149895da51f
DiyanshuTiwaari <sontitiwa569@gmail.com>
2024-10-12 12:42:23 +0530

    updated alyout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import localFont from "next/font/local";
+
 import "./globals.css";
 import AuthProvider from "../context/AuthProvider";
 
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className={myFont.variable}>
+        <html lang="en" >
             <AuthProvider>
                 <body className="bg-black">
                     {children}
