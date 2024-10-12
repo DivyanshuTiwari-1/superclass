ef5e20938dbe13e78c28f60208a681f2c4e9237b
DiyanshuTiwaari <sontitiwa569@gmail.com>
2024-10-12 23:53:42 +0530

    updatedauthoptions

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,48 +1,36 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const handler = NextAuth({
-    // Configure the Google provider
+// Define the auth options
+export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
         }),
     ],
-
-    // Enable JWT sessions
     session: {
         jwt: true,
     },
-
-
-
-    // Callbacks for handling JWT and session data
     callbacks: {
-        // This callback is called whenever a new JWT token is created
         async jwt({ token, user, account }) {
-            // If it's the first sign in, store the user's name and email in the token
             if (user) {
-                console.log("User:", user);
                 token.name = user.name;
                 token.email = user.email;
                 token.accesstoken = account.access_token;
             }
             return token;
         },
-
-        // This callback is called whenever a session is checked
         async session({ session, token }) {
-            // Send user name and email to the session
             session.accesstoken = token.accesstoken;
             session.user.name = token.name;
             session.user.email = token.email;
             return session;
         },
-
-        secret: process.env.NEXTAUTH_URL
     },
-});
+    secret: process.env.NEXTAUTH_SECRET
+};
 
-export { handler as GET, handler as POST,handler as authOptions }
+// Export the handler for NextAuth
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
